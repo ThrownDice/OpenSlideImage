13e7724aa0eeac1e6763c76a7a353b6e8b857a6e
KANG JI HYEON <kiwlgus1@korea.ac.kr>
2015-06-26 12:18:51 +0900

    150626 first commit

---
+++ js/index.js
@@ -0,0 +1,54 @@
+/**
+ * Created by TD on 2015-06-20.
+ */
+$(function(){
+   SlideMgr.setConfig({
+       container : $('.canvas_test').get(0),
+       start : 7,
+       end : 100,
+       prefix : 'img/Journey.To.The.Edge.Of.The.Universe.2008.720p.BluRay.x264-REQ [PublicHD].avi-',
+       extendType : 'jpg',
+       scrollColor : 'white',
+       render_interval : 30,
+       animate_interval : 10,
+       callback : function(){
+
+       }
+   });
+
+    SlideMgr.addElement({
+        innerHTML : 'test1 </br> test2',
+        show_frame : 1,
+        hide_frame : 30,
+        attr : {
+            left : '20px',
+            top : '30px',
+            color : 'white'
+        }
+    });
+
+    SlideMgr.addElement({
+        innerHTML : 'My Vision.......</br> is work together',
+        show_frame : 35,
+        hide_frame : 45,
+        attr : {
+            fontSize : '15pt',
+            color : 'white',
+            left : '300px',
+            top : '300px'
+        }
+    });
+
+    SlideMgr.addElement({
+        innerHTML : 'My hobby....</br> etc1...... Blah Blah~~ </br> OMG!!',
+        show_frame : 45,
+        hide_frame : 55,
+        attr : {
+            fontSize : '12pt',
+            color : 'white',
+            left : '600px',
+            top : '400px'
+        }
+    });
+
+});
